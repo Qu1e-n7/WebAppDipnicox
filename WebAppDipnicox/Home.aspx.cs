fd259742eddb6ff5d8260cc66386ee756aa3614f
Walter Vega <waltveg21@outlook.com>
2023-05-22 19:38:42 -0500

    css xd

--- WebAppDipnicox/Home.aspx.cs
+++ WebAppDipnicox/Home.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,7 @@ namespace WebAppDipnicox
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
--- WebAppDipnicox/Site1.Master.designer.cs
+++ WebAppDipnicox/Site1.Master.designer.cs
@@ -24,6 +24,15 @@ namespace WebAppDipnicox
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
+        /// Control ReDatos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ReDatos;
+
+        /// <summary>
         /// Control form1.
         /// </summary>
         /// <remarks>
